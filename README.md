122d6c7a10a1129d5a63c3c69d341c18c87452df
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-05-15 12:07:11 -0700

    progress

---
+++ README.md
@@ -0,0 +1,26 @@
+# code_quiz
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+
+// First user click Start Quiz
+ - timecounter start giving 75 sec
+ - first question pop out with 4 multiple options.
+
+ // Second uses clicks answer
+ - if answer is WRONG a message "Wrong!" will display on the bottom part.
+- Also, it will trigger the next question.
+- if answer is WRONG a message "Correct" will display on the bottom part.
+
+After 5 question...
+
+A last message with the score and a form to enter initials.
---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+// window.alert("This is an alert! JavaScript is running!");
+
+// var starQuiz = function() {
+//     $('#star-quiz').on("click", function() {
+
+//     }
+//     )}
+
+
+
+
+var btn = function() {
+    [
+    {
+      id: 1,
+      name: "answer-1-3",
+      type: "button",
+      status: "Correct",
+    },
+    {
+      id: 2,
+      name: "answer-2-3",
+      type: "button",
+      status: "Correct",
+    },
+    {
+      id: 3,
+      name: "answer-3-4",
+      type: "button",
+      status: "Correct",
+    },
+    {
+        id: 4,
+        name: "answer-4-3",
+        type: "button",
+        status: "Correct",  
+    },
+    {
+        id: 5,
+        name: "answer-5-4",
+        type: "button",
+        status: "Correct",  
+    }
+
+    document.querySelector('#answer-1-3', '#answer-2-3') {
+    
+        addEventListener('click', btn); {
+            console.log('correct')
+        ;}
+        
+      }
+
+  ];}
+
+  
+  
+  
+
+    
+
+// // create DELETE button
+// var deleteButtonEl = document.createElement("button");
+// deleteButtonEl.addEventListener("click", deleteButtonEl);
+
+// deleteButtonEl.textContent = "Delete";
+// deleteButtonEl.className = "btn delete-btn";
+// deleteButtonEl.setAttribute("data-task-id", taskId);
+
+// actionContainerEl.appendChild(deleteButtonEl);
+
+// var statusSelectEl = document.createElement("select");
+// statusSelectEl.className = "select-status";
+// statusSelectEl.setAttribute("name", "status-change");
+// statusSelectEl.setAttribute("data-task-id", taskId);
+
+// actionContainerEl.appendChild(statusSelectEl);
+
+// var statusChoices = ["to do", "In Progress", "Completed"];
+// for (var i = 0; i < statusChoices.length; i++) {
